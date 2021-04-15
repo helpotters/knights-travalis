2f3138aa48d82d7e704edd7613ad07bc631d3bba
Paul Lemus <paullemus@protonmail.com>
2021-04-15 13:18:27 -0700

    Added rspec testing

---
+++ spec/knight_spec.rb
@@ -0,0 +1,10 @@
+require './lib/knight'
+
+describe Knight do
+  describe '#knights_move' do
+    it 'moves a knight piece and return how many moves it took' do
+      knight = Knight.new
+      expect(knight.knights_move([0, 0], [2, 1])).to eql('it took 2 moves.')
+    end
+  end
+end
